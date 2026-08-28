cf25fa5675a0a88e0f2173df6d2e32a83aa39a4c
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-08-28 16:15:34 +0530

    add the server connection configuration with database

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+async function ConnectToDB(){
+
+    await mongoose.connect(process.env.MONGODB_URI)
+    .then (()=>{
+        console.log(' MongoDB connected');
+  }) .catch((err)=>{
+    console.error(' MongoDB connection error:', err.message);
+    process.exit(1); // stop the server if DB fails — no point running without it
+  });
+};
+
+
+module.exports = connectoDB;
